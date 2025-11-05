d1610bea51237f11e6d59a86cc3b171a17a7451f
YashD <yash.dhavale2010@gmail.com>
2025-11-05 15:08:46 -0500

    Adding temp secrets

---
+++ Proj2/src/frontend/src/secrets/users.js
@@ -0,0 +1,11 @@
+// src/secrets/users.js
+
+const users = [
+  { email: "customer@test.com", password: "customer123", name: "John Doe" },
+  { email: "jane@test.com", password: "jane123", name: "Jane Smith" },
+  { email: "alex@test.com", password: "alex123", name: "Alex Brown" },
+  { email: "emma@test.com", password: "emma123", name: "Emma Davis" },
+  { email: "liam@test.com", password: "liam123", name: "Liam Wilson" },
+];
+
+export default users;
