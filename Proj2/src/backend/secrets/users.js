d07868b57c0d560c4bd3ed0ba76db3fd33f435ea
YashD <yash.dhavale2010@gmail.com>
2025-11-07 13:27:28 -0500

    #7: Removing secrets

--- Proj2/src/backend/secrets/users.js
+++ Proj2/src/backend/secrets/users.js
@@ -1,12 +1,2 @@
 module.exports = [
-  {
-    "name": "Yash",
-    "email": "yash@gmail.com",
-    "password": "Barca"
-  },
-  {
-    "name": "Shreeya Ranwadkar",
-    "email": "spranwad@ncsu.edu",
-    "password": "Shreeya123@"
-  }
 ];
