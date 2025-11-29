dd68f6b40c1dee0506ba6b6d4ce5dd86cc555ca1
vince@CFS <vincentfschuang@gmail.com>
2025-11-28 22:57:44 -0500

    update restaurants' login data

--- Proj2/src/backend/secrets/users.js
+++ Proj2/src/backend/secrets/users.js
@@ -7,6 +7,69 @@ module.exports = [
     "restaurantName": "GreenBite Cafe"
   },
   {
+    "name": "Eastside Deli Owner",
+    "email": "eastsideD@gmail.com",
+    "password": "test",
+    "accountType": "restaurant",
+    "restaurantName": "Eastside Deli"
+  },
+  {
+    "name": "Oak Street Bistro",
+    "email": "OSB@gmail.com",
+    "password": "test",
+    "accountType": "restaurant",
+    "restaurantName": "Oak Street Bistro"
+  },
+  {
+    "name": "Triangle BBQ Co.",
+    "email": "BBQ@gmail.com",
+    "password": "test",
+    "accountType": "restaurant",
+    "restaurantName": "Triangle BBQ Co."
+  },
+  {
+    "name": "Village Noodle Bar",
+    "email": "Noodle@gmail.com",
+    "password": "test",
+    "accountType": "restaurant",
+    "restaurantName": "Village Noodle Bar"
+  },
+  {
+    "name": "Capital City Tacos",
+    "email": "taco@gmail.com",
+    "password": "test",
+    "accountType": "restaurant",
+    "restaurantName": "Capital City Tacos"
+  },
+  {
+    "name": "Hillside Kitchen",
+    "email": "hillside@gmail.com",
+    "password": "test",
+    "accountType": "restaurant",
+    "restaurantName": "Hillside Kitchen"
+  },
+  {
+    "name": "Roosevelt Oyster House",
+    "email": "roosevelt@gmail.com",
+    "password": "test",
+    "accountType": "restaurant",
+    "restaurantName": "Roosevelt Oyster House"
+  },
+  {
+    "name": "Southside Pizza Lab",
+    "email": "southsidepizza@gmail.com",
+    "password": "test",
+    "accountType": "restaurant",
+    "restaurantName": "Southside Pizza Lab"
+  },
+  {
+    "name": "UrbanEats",
+    "email": "urbaneats@gmail.com",
+    "password": "test",
+    "accountType": "restaurant",
+    "restaurantName": "UrbanEats"
+  },
+  {
     "name": "Test",
     "email": "test@gmail.com",
     "password": "test",
